4af580fe9a3616c52beb9e9a13775d19e8dc569a
itsshawondevs <shawonuser24@gmail.com>
2024-03-03 21:21:33 +0600

    registration form update

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,8 @@ function App() {
   let [numberData, setNumberData] = useState('')
   let [addressData, setAddressData] = useState('')
 
+  let [block, setBlock] = useState(false)
+
   let handleInput = (e) => {
     let {name, value} = e.target
     setText({...text, [name]: value})
@@ -71,7 +73,7 @@ function App() {
     
     // data transfer registration form to Bio-data
 
-    if(text.username && text.email && text.number && text.address && text.password){
+    if(text.username && text.email && text.email.match(emailRegex) && text.number.match(numberRegex) && text.number && text.address && text.password){
 
       nameData = text.username
       setNameData(nameData)
@@ -84,42 +86,56 @@ function App() {
   
       addressData = text.address
       setAddressData(addressData)
+      setBlock(true)
     }
 
+
+  }
+
+  let handleBack = () =>{
+    setBlock(false)
   }
 
   return (
     <>
       <div className="container">
-        <form action="" method="">
+        {
+          block ? 
+
+          // Registrasion bio-data here
+            <div className="reg_fill">
+              <h1 className="bio_heading">Registration Bio-Data</h1>
+              <div className="info_box">
+                <h2><span>Username:</span> {nameData}</h2>
+                <h2><span>Email:</span> {emailData}</h2>
+                <h2><span>Number:</span> {numberData}</h2>
+                <h2><span>Address:</span> {addressData}</h2>
+
+                <button onClick={handleBack}>back</button>
+              </div>
+            </div>
+          :
+          
+          //Registration form here
           <div className="reg_form">
-            <h1 className="heading">Registrasion form</h1>
-            <input type='text' name='username' onChange={handleInput} placeholder='Name'/>
-            <p className="error">{nameErr}</p>
-            <input type='text' onChange={handleInput} name='email' placeholder='email'/>
-            <p className="error">{emailErr}</p>
-            <input type='number' onChange={handleInput} name='number' placeholder='number'/>
-            <p className="error">{numberErr}</p>
-            <input type="text" onChange={handleInput} name='address' placeholder="address"/>
-            <p className="error">{addressErr}</p>
-            <input type='password' onChange={handleInput} name='password' placeholder='password'/>
-            <p className="error">{passErr}</p>
-            <button onClick={handleSubmit}>submit</button>
+            <form action="" method="">
+              <h1 className="heading">Registrasion form</h1>
+              <input type='text' name='username' onChange={handleInput} placeholder='enter your name'/>
+              <p className="error">{nameErr}</p>
+              <input type='text' onChange={handleInput} name='email' placeholder='enter your email'/>
+              <p className="error">{emailErr}</p>
+              <input type='number' onChange={handleInput} name='number' placeholder='enter your number'/>
+              <p className="error">{numberErr}</p>
+              <input type="text" onChange={handleInput} name='address' placeholder="enter your address"/>
+              <p className="error">{addressErr}</p>
+              <input type='password' onChange={handleInput} name='password' placeholder='enter password'/>
+              <p className="error">{passErr}</p>
+              <button onClick={handleSubmit}>submit</button>
+            </form>
           </div>
-        </form>
 
-
-        {/* Registrasion bio-data here */}
-
-        <div className="reg_fill">
-          <h1 className="bio_heading">Registration Bio-Data</h1>
-          <div className="info_box">
-            <h2>Username: {nameData}</h2>
-            <h2>Email: {emailData}</h2>
-            <h2>Number: {numberData}</h2>
-            <h2>Address: {addressData}</h2>
-          </div>
-        </div>
+          
+        }
 
       </div>
     </>
